e2cd7349e830c3e420dc89053576acb5ae77757a
Fell <coldcity@gmail.com>
2021-03-10 19:06:11 +0000

    Fixed typo, added version to About screen

--- transed/FormAbout.Designer.cs
+++ transed/FormAbout.Designer.cs
@@ -39,11 +39,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(53, 9);
+            this.label1.Location = new System.Drawing.Point(14, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(142, 37);
+            this.label1.Size = new System.Drawing.Size(221, 37);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Transed";
+            this.label1.Text = "Transed V1.0";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // label2
